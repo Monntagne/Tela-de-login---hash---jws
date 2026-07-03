9248bec0f89c46d6c606548c6afcf95efae45e9a
FERNANDA NALON PIRES SAUAIA <fernanda.npsauaia@senacsp.edu.br>
2026-07-03 20:08:30 -0300

    feat: criando cookies e sessions

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,4 @@
-import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { Injectable, ServiceUnavailableException, UnauthorizedException } from '@nestjs/common';
 import { compare } from 'bcrypt';
 import { LoginDto } from './dto/login.dto';
 import { Usuario } from './interface/usuario.interface';
@@ -40,6 +40,36 @@ export class AuthService {
                 nome: usuario.nome,
                 email: usuario.email,
             },
+        };
+    }
+
+    async loginComSession(loginDto:LoginDto){
+        const {email, senha} = loginDto;
+        const resultado = await this.databaseService.query(
+            `
+            SELECT id, nome, email, senha
+            FROM usuario
+            WHERE email = ?
+            `,
+            [email]
+        );
+        const usuarios = resultado as Usuario[];
+        const usuario = usuarios[0];
+
+        if(!usuario) {
+            throw new UnauthorizedException('Email ou senha inválidos');
+        }
+
+        const senhaValida = await compare(senha, usuario.senha);
+
+        if(!senhaValida) {
+            throw new UnauthorizedException('Email ou senha inválidos!')
+        }
+
+        return {
+            id: usuario.id,
+            nome: usuario.nome,
+            email: usuario.email
         };
     }
 }
--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,27 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common'; 
+import session from 'express-session';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
-  app.enableCors();
+  app.enableCors({
+    origin: 'http://127.0.0.1:5500',
+    credentials: true
+  });
+
+  app.use(
+    session({
+      secret: process.env.SESSION_SECRET!,
+      resave: false,
+      saveUninitialized: false,
+      cookie: {
+        maxAge: 1000 * 60 * 60
+      }
+    })
+  )
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
